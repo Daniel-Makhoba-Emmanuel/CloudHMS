e76fcdcaee9905bf8aa64e2461d04229420d5449
Daniel M.E <iamdanielemmanuel22@gmail.com>
2024-09-17 16:23:40 +0100

    added entrypoint vnet

--- Departments/Global/main.tf
+++ Departments/Global/main.tf
@@ -15,3 +15,42 @@ resource "azurerm_redis_cache" "Global-redis" {
   sku_name = "Basic"
   family = "C"
 }
+
+resource "azurerm_virtual_network" "Global-vnet" {
+  name = "${var.prefix}-vnet"
+  location = azurerm_resource_group.GlobalRG.location
+  resource_group_name = azurerm_resource_group.GlobalRG.name
+  address_space = [ "10.0.0.0/24" ]
+}
+
+resource "azurerm_subnet" "Globalsubnet1" {
+  name = "${var.prefix}-subnet1"
+  resource_group_name = azurerm_resource_group.GlobalRG.name
+  virtual_network_name = azurerm_virtual_network.Global-vnet.name
+  address_prefixes = [ "10.0.0.0/26" ]
+  
+}
+
+resource "azurerm_subnet" "Globalsubnet2" {
+  name = "${var.prefix}-subnet2"
+  resource_group_name = azurerm_resource_group.GlobalRG.name
+  virtual_network_name = azurerm_virtual_network.Global-vnet.name
+  address_prefixes = [ "10.0.0.64/26" ]
+  
+}
+
+resource "azurerm_subnet" "Globalsubnet3" {
+  name = "${var.prefix}-subnet3"
+  resource_group_name = azurerm_resource_group.GlobalRG.name
+  virtual_network_name = azurerm_virtual_network.Global-vnet.name
+  address_prefixes = [ "10.0.0.128/26" ]
+  
+}
+
+resource "azurerm_subnet" "Globalsubnet4" {
+  name = "${var.prefix}-subnet4"
+  resource_group_name = azurerm_resource_group.GlobalRG.name
+  virtual_network_name = azurerm_virtual_network.Global-vnet.name
+  address_prefixes = [ "10.0.0.192/26" ]
+  
+}

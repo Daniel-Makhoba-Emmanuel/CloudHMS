d99e46d3589934db9f499e0003b35f74d5bf5d59
Daniel M.E <iamdanielemmanuel22@gmail.com>
2024-09-23 18:10:58 +0100

    added folders into storage account

--- Departments/Hr/main.tf
+++ Departments/Hr/main.tf
@@ -8,10 +8,12 @@ resource "azurerm_resource_group" "HR-RG" {
   location = var.infra_location
 }
 
+############### Storage Account structure ########################
+
 resource "azurerm_storage_account" "HR-st" {
   name = "${lower(var.prefix)}st${random_integer.Storage_suffix.result}"
-  resource_group_name = azurerm_resource_group.HrRG.name
-  location = azurerm_resource_group.HrRG.location
+  resource_group_name = azurerm_resource_group.HR-RG.name
+  location = azurerm_resource_group.HR-RG.location
   account_tier = "Standard"
   account_replication_type = "LRS"
 
@@ -20,6 +22,115 @@ resource "azurerm_storage_account" "HR-st" {
   }
   
 }
+
+resource "azurerm_storage_container" "HR-employee-folder" {
+  name = "${lower(var.prefix)}-employees-folder"
+  storage_account_name = azurerm_storage_account.HR-st.name
+}
+
+resource "azurerm_storage_blob" "Personnel-Information" {
+  name = "Personnel-information/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-employee-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Employment-records" {
+  name = "Employment-records/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-employee-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Benefits" {
+  name = "Benefits/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-employee-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Time-and-attendance" {
+  name = "Time-and-attendance/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-employee-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Training-and-Development" {
+  name = "Training-and-Development/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-employee-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Exit-Documents" {
+  name = "Exit-Documents/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-employee-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_container" "HR-administrative-folder" {
+  name = "${lower(var.prefix)}-administrative-folder"
+  storage_account_name = azurerm_storage_account.HR-st.name
+}
+
+resource "azurerm_storage_blob" "Policies-and-Procedures" {
+  name = "Policies-and-Procedures/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-administrative-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Legal-Documents" {
+  name = "Legal-Documents/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-administrative-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Payroll-Records" {
+  name = "Payroll-Records/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-administrative-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Recruitment-and-Selection" {
+  name = "Recruitment-and-Selection/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-administrative-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Performance-Management" {
+  name = "Performance-Management/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-administrative-folder.name
+  type = "Block"
+  
+}
+
+resource "azurerm_storage_blob" "Employee-Relations" {
+  name = "Employee-Relations/"
+  storage_account_name = azurerm_storage_account.HR-st.name
+  storage_container_name = azurerm_storage_container.HR-administrative-folder.name
+  type = "Block"
+  
+}
+
+##################### Storage Account Structure End ##############
+
 
 resource "azurerm_consumption_budget_resource_group" "HR-Budget" {
   name = "${var.prefix} Budget"
